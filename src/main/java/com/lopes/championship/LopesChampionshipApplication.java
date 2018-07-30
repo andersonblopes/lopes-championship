69786bbcedb81e936ebe3cf75e5a5ac1903928cf
Anderson Lopes <anderson.Lopes@adtalem.com>
2018-07-30 19:48:37 -0300

    First commit.

---
+++ src/main/java/com/lopes/championship/LopesChampionshipApplication.java
@@ -0,0 +1,12 @@
+package com.lopes.championship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LopesChampionshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LopesChampionshipApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lopes/championship/LopesChampionshipApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lopes.championship;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LopesChampionshipApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
