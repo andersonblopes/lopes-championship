e2a536c6130b2efba7a815de8b593021ef76b360
Anderson Lopes <anderson.Lopes@adtalem.com>
2018-07-30 20:07:27 -0300

    Includes sql file.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into team (id, name)values(null,"São Paulo Futebol Clube");
+insert into team (id, name)values(null,"Ceará Sporting Club");
+insert into team (id, name)values(null,"Barcelona Futebol Club");
+insert into team (id, name)values(null,"Real Madrid");
+insert into team (id, name)values(null,"Bayer de Munich");
